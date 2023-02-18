b8b54715802d6ed11317460f0e8307834b6c1e51
sent7xo <deathsnake4@gmail.com>
2023-02-18 15:35:08 +0700

    more recent initcmd.bat

--- scripts/initcmd.bat
+++ scripts/initcmd.bat
@@ -3,22 +3,25 @@ prompt $E[92m$P$E[90m$E[m$S$E]9;12$E\$_$G$S
 
 doskey pwd  = cd
 doskey cd   = cd /d $*
-doskey j    = cd /d "D:\programming\java"
-doskey game = cd /d "D:\programming\c_cpp\game\"
-:: doskey game = cd /d "D:\programming\java\software\gl"
+doskey prog = cd /d "D:\programming\"
+doskey j    = cd /d "D:\programming\java\"
+doskey cpp  = cd /d "D:\programming\c_cpp\"
 
 doskey n = nvy $*
 
 doskey jc  = javac $1.java $T java $1
-REM doskey dir = dir /w $*
-doskey dir = java -cp "D:\programming\java\software\mydir" MyDir $*
-doskey initcpp = "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+:: doskey dir = dir /w $*
+:: doskey dir = java -cp "D:\programming\java\software\mydir" MyDir $*
+doskey dir = D:\programming\c_cpp\bin\dir.exe $*
+doskey initcpp = "D:\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 doskey mkdircpp = mkdir $1 $T copy D:\exec\build_template.bat $1\build.bat
-doskey run = .\build\code
 
 doskey rr  = D:\exec\initcmd.bat
 doskey ei  = D:\Nvy\Nvy.exe D:\exec\initcmd.bat
 
-doskey mycmd = echo j, game: D:\programming\ ^& echo jc: compile and run java ^& echo initcpp, mkdircpp: vcvars and make cpp dir ^& echo rr, ei: reload and edit init
+doskey myhelp = echo j, game: D:\programming\ ^& echo jc: compile and run java ^& echo initcpp, mkdircpp: vcvars and make cpp dir ^& echo rr, ei: reload and edit init
+
+doskey flask-on  = venv\Scripts\activate
+doskey flask-off = venv\Scripts\deactivate
 
 :: cls
